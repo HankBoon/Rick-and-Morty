00f03eb5667a9eec659c515e2eb8d5c6f1e50171
Hauke <hank.la.boon@gmail.com>
2024-11-14 16:09:57 +0100

    add search function

--- index.js
+++ index.js
@@ -10,24 +10,36 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-const urlApi = "https://rickandmortyapi.com/api/character?page=";
+const urlApi = "https://rickandmortyapi.com/api/character";
 
 // States
 let maxPage = 1;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
 // Fetch and Render Character Cards
 
-async function fetchCharacters(page) {
-  const response = await fetch(urlApi + page);
+async function fetchCharacters(pageUrl, searchqueryOrPageNumber) {
+  const response = await fetch(urlApi + pageUrl + searchqueryOrPageNumber);
   const data = await response.json();
+  // console.log("urlApi+param:", urlApi + );
   return data;
 }
 
 async function renderCharacters() {
-  const apiList = await fetchCharacters(page);
+  const apiList = await fetchCharacters("?page=", page);
   cardContainer.innerHTML = ``;
+  apiList.results.forEach((character) => {
+    const CharacterCard = createCharacterCard(character);
+    cardContainer.append(CharacterCard);
+  });
+}
+
+async function renderSingleChar(query) {
+  const apiList = await fetchCharacters("?name=", query);
+  cardContainer.innerHTML = ``;
+  console.log("?name=", query);
+  console.log("names:", apiList);
   apiList.results.forEach((character) => {
     const CharacterCard = createCharacterCard(character);
     cardContainer.append(CharacterCard);
@@ -37,16 +49,18 @@ async function renderCharacters() {
 // Pagination
 
 async function setPages() {
-  const apiList = await fetchCharacters(page);
+  // console.log("foo", `?page=${page}`);
+  const apiList = await fetchCharacters("?page=", page);
   maxPage = apiList.info.pages;
   pagination.textContent = `${page}/${maxPage}`;
+  // console.log("apiList: ", apiList);
 }
 
 nextButton.addEventListener("click", (event) => {
   event.preventDefault();
   if (page < maxPage) {
     page++;
-    renderCharacters();
+    renderCharacters(page);
     setPages();
     scrollTo(0, 0);
   }
@@ -56,10 +70,19 @@ prevButton.addEventListener("click", (event) => {
   event.preventDefault();
   if (page > 1) {
     page--;
-    renderCharacters();
+    renderCharacters(page);
     setPages();
     scrollTo(0, 0);
   }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log(event.target.elements);
+  searchQuery = event.target.elements.query.value;
+  renderSingleChar(searchQuery);
+  event.target.reset();
+  searchQuery.focus;
 });
 
 //Initial Page Setup
