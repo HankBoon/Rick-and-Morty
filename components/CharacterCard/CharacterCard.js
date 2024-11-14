f7c5535d3d29b203d9d9cf5cf3d1318458794512
Lars Sperling <155863133+SLworld13@users.noreply.github.com.>
2024-11-14 10:49:44 +0100

    crerated charachter card

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,25 @@
-export function createCharacterCard() {}
+export function createCharacterCard(characterList) {
+  const CharacterCard = document.createElement("li");
+  CharacterCard.classList.add("character-card");
+  CharacterCard.innerHTML = `
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${characterList.results[2].image}"
+              alt="${characterList.results[0].name}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${characterList.results[0].name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${characterList.results[0].status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${characterList.results[0].type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${characterList.results[2].episode.length}</dd>
+            </dl>
+          </div>`;
+  return CharacterCard;
+}
